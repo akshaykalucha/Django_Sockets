789686a4efb6c938ed5040e17514a981af8bda2a
akshay kalucha <akshaykalucha@gmail.com>
2020-07-19 02:24:04 +0530

    updates

--- graphsocket/consumer.py
+++ graphsocket/consumer.py
@@ -82,3 +82,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
 
     # Receive message from room group
+    async def chat_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
