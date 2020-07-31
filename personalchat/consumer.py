eaa7409d36d70ee4ae0fe94998c2aaedb9b853f0
akshay kalucha <akshaykalucha@gmail.com>
2020-07-31 16:43:26 +0530

    updates

--- personalchat/consumer.py
+++ personalchat/consumer.py
@@ -29,6 +29,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
         r.hmset("channels", channelInfo)
 
 
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
 
 
 
