1b63275cdf60a200c097646f6fde12c724799dfd
akshay kalucha <akshaykalucha@gmail.com>
2020-08-14 15:21:16 +0530

    sockets views

--- graphsocket/views.py
+++ graphsocket/views.py
@@ -4,7 +4,13 @@ import datetime
 import requests
 # Create your views here.
 
-
+def index(request):
+    session = request.COOKIES['servercookie']
+    print(session, "A cookie got by server")
+    response = render(request, "graphsocket/index.html")
+    response.set_cookie('servercookie', "this is from server", samesite="Strict", domain='127.0.0.1', path="/graph/")
+    response.set_cookie("serverGlobal", "this is for the website but available to all paths")
+    return response
 
 def chathome(request):
     return render(request, 'graphsocket/chathome.html')
