659b173b7f5ecf26cc5f052a50a0027dcfc48673
akshay kalucha <akshaykalucha@gmail.com>
2020-09-12 23:22:00 +0530

    some updates

--- graphsocket/webserver_client.py
+++ graphsocket/webserver_client.py
@@ -73,10 +73,10 @@ def encode_to_UTF8(data):
         return False
 
 
-def try_decode_UTF8(data):
-    try:
-        return data.decode('utf-8')
-    except UnicodeDecodeError:
-        return False
-    except Exception as e:
-        raise(e)
+# def try_decode_UTF8(data):
+#     try:
+#         return data.decode('utf-8')
+#     except UnicodeDecodeError:
+#         return False
+#     except Exception as e:
+#         raise(e)
