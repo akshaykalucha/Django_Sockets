8393d92183d38d101b4ae2482e0268c652a070e7
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-01-13 19:20:57 +0530

    updates

--- graphsocket/core.py
+++ graphsocket/core.py
@@ -148,3 +148,9 @@ def register(**args):
     if pattern is not None and not pattern.startswith('(?i)'):
         args['pattern'] = '(?i)' + pattern
 
+    if "disable_edited" in args:
+        del args['disable_edited']
+
+    if "ignore_unsafe" in args:
+        del args['ignore_unsafe']
+
