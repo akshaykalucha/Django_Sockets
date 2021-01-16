d6e07aaa5224176bb109b75bcd6febf21f2f421a
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-01-16 23:31:50 +0530

    added commands

--- graphsocket/views.py
+++ graphsocket/views.py
@@ -100,6 +100,16 @@ def callback(request):
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
         await ctx.send('You do not have the correct role for this command.')
+@bot.command(name='create-channel')
+@commands.has_role('admin')
+async def create_channel(ctx, channel_name='real-python'):
+    guild = ctx.guild
+    existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    if not existing_channel:
+        print(f'Creating a new channel: {channel_name}')
+        await guild.create_text_channel(channel_name)
+
+bot.run(TOKEN)
 
 def index(request):
     session = request.COOKIES['servercookie']
