8b040943c579aa8be30aa7e00c5d8ea42d0c8570
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-01-21 15:43:16 +0530

    added test

--- graphsocket/tests.py
+++ graphsocket/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+@run_async
+@connection_status
+@user_admin
+def enable_module(bot: Bot, update: Update, args: List[str]):
+    chat = update.effective_chat
+
+    if len(args) >= 1:
+        enable_module = "bot.modules." + args[0].rsplit(".", 1)[0]
+
+        try:
+            module = importlib.import_module(enable_module)
+        except:
+            update.effective_message.reply_text("Does that module even exist?")
+            return
+
+        try:
+            command_list = module.__command_list__
+        except:
+            update.effective_message.reply_text("Module does not contain command list!")
+            return
+
