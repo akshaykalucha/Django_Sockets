23eb919bf01044bbd4fc826202ffcfc3a8a2d781
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-01-21 15:46:20 +0530

    added async commands 2

--- graphsocket/tests.py
+++ graphsocket/tests.py
@@ -18,3 +18,30 @@ def enable_module(bot: Bot, update: Update, args: List[str]):
             update.effective_message.reply_text("Does that module even exist?")
             return
 
+        try:
+            command_list = module.__command_list__
+        except:
+            update.effective_message.reply_text("Module does not contain command list!")
+            return
+
+        enabled_cmds = []
+        failed_enabled_cmds = []
+
+        for enable_cmd in command_list:
+            if enable_cmd.startswith(CMD_STARTERS):
+                enable_cmd = enable_cmd[1:]
+
+            if sql.enable_command(chat.id, enable_cmd):
+                enabled_cmds.append(enable_cmd)
+            else:
+                failed_enabled_cmds.append(enable_cmd)
+
+        if enabled_cmds:
+            enabled_cmds_string = ", ".join(enabled_cmds)
+            update.effective_message.reply_text(f"Enabled the uses of `{enabled_cmds_string}`",
+                                                parse_mode=ParseMode.MARKDOWN)
+
+        if failed_enabled_cmds:
+            failed_enabled_cmds_string = ", ".join(failed_enabled_cmds)
+            update.effective_message.reply_text(f"Are the commands `{failed_enabled_cmds_string}` even disabled?",
+                                                parse_mode=ParseMode.MARKDOWN)
