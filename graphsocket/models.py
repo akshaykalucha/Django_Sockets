a6ce8336911fc70f36ad1b53eb4ea60bb3e61cf7
akshay kalucha <akshaykalucha@gmail.com>
2021-03-05 00:44:17 +0530

    model added

--- graphsocket/models.py
+++ graphsocket/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+def explore_data(df):
+    print("Number of Instances and Attributes:", df.shape)
+    print('\n')
+    print('Dataset columns:',df.columns)
+    print('\n')
+    print('Data types of each columns: ', df.info())
+    
+# Checking for duplicates
+def checking_removing_duplicates(df):
+    count_dups = df.duplicated().sum()
+    print("Number of Duplicates: ", count_dups)
+    if count_dups >= 1:
+        df.drop_duplicates(inplace=True)
+        print('Duplicate values removed!')
+    else:
+        print('No Duplicate values')
+    
+
+# Split training and validation set
+def read_in_and_split_data(data, target):
+    X = data.drop(target, axis=1)
+    y = data[target]
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=0)
+    return X_train, X_test, y_train, y_test
